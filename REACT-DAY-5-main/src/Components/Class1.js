600bf15d2029262f3c4b33e1f24b18e3346ffae8
GeonesVarghese <108513934+GeonesVarghese@users.noreply.github.com>
2023-09-01 15:16:39 +0530

    Delete REACT-DAY-5-main directory

--- REACT-DAY-5-main/src/Components/Class1.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react'
-
-export default class Class1 extends Component {
-  render() {
-    return (
-      <div>Class1</div>
-    )
-  }
-}
--- REACT-DAY-5-main/src/Components/Class3.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react'
-
-export default class Class3 extends Component {
-  render() {
-    return (
-      <div>Class3</div>
-    )
-  }
-}
